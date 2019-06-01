ebc5749c12dab96d98bb4c126a46ab75c588ad75
Victor Lazaro <vlazaro@hotmail.com>
2019-06-01 12:59:57 -0400

    Projecto Spring boot Data

---
+++ JessicaData/src/main/java/com/linkia/jessica/data/jessicarepository/JessicarepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.linkia.jessica.data.jessicarepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JessicarepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JessicarepositoryApplication.class, args);
+	}
+
+}
