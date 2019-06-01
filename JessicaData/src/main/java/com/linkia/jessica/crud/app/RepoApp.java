ef922390b69104739c7fe2e5954e89819120df44
Victor Lazaro <vlazaro@hotmail.com>
2019-06-01 16:38:15 -0400

    4 commit

--- JessicaData/src/main/java/com/linkia/jessica/crud/app/RepoApp.java
+++ JessicaData/src/main/java/com/linkia/jessica/crud/app/RepoApp.java
@@ -1,4 +1,4 @@
-package com.linkia.jessica.app;
+package com.linkia.jessica.crud.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
